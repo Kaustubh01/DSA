e814d065b9d26b3ec23f4c367a3c910239fcab0a
Kaustubh01 <kaustubhmayekar02@gmail.com>
2022-10-10 20:13:27 +0530

    Added display and main function

--- linked_list.c
+++ linked_list.c
@@ -3,7 +3,7 @@
 typedef struct Node{
 	int data;
 	struct Node* next;
-};
+}Node;
 
 Node* start = NULL;
 
@@ -15,14 +15,38 @@ void insert_end(int key){
 	if(start == NULL){
 		start = node;
 	}
-	else if(start->next == NULL){
-		start->next=node;
-	}
+
 	else{
 			t = start;
-			while(t != NULL){
+			while(t->next != NULL){
 				t=t->next;
 			}
 			t->next=node;
+	}
+}
+
+void display(){
+	Node* t = start;
+	printf("\nElements of linked list are:\n");
+	while(t != NULL){
+		printf("%d\t",t->data);
+		t=t->next;
+	}
+}
+
+void main(){
+	int n,key;
+	while(1){
+		printf("\nEnter a number:");
+		scanf("%d",&n);
+		switch(n){
+			case 1:
+				printf("\nEnter a number to insert in the linked list");
+				scanf("%d",&key);
+				insert_end(key);
+				break;
+			case 2:display();break;
+			case 3:exit(0);break;
+		}
 	}
 }
